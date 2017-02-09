1cc0773d199c4f26bae0afd12d785ba72adc6a43
ErikBoesen <erik.boesen@gmail.com>
2017-02-08 22:28:37 -0500

    Improve design

--- app/app.py
+++ app/app.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
 import tbapi
-import datetime
+from datetime import datetime
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 TEAM = 1418
+STR_TEAM = 'frc1418'
 
 tba = tbapi.TBAParser(TEAM, 'watchvictis', 0.1)
 
+"""
 def get_tba_data():
     data = {}
-    for i in tba.get_team_events(TEAM):
-        if datetime(i.start_date) < datetime.now() and datetime.now() < datetime(i.end_date):
+    for i in tba.get_team_events(STR_TEAM):
+        print(i.start_date)
+        if datetime(i.start_date.split('-')) < datetime.now() and datetime.now() < datetime(i.end_date.split('-')):
             event = i
             break
         event = {}
     event['matches'] = tba.get_team_event_matches(TEAM, event.key)
     return data
-
+"""
 
 @app.route('/')
 def index():
@@ -29,7 +33,7 @@ def index():
 
 @socketio.on('event', namespace='/req')
 def message(message):
-    emit('response', get_tba_data())
+    pass
 
 
 @socketio.on('connect', namespace='/req')
@@ -41,6 +45,10 @@ def connect():
 def disconnect():
     print('Client disconnected.')
 
+
+while True:
+    emit('data', get_tba_data())
+    time.sleep(100)
 
 if __name__ == '__main__':
     socketio.run(app)
